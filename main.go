028bdd62655d3ad1a31f015cdf9580727b0fed61
Joao Paulo <joaopaulosc06@gmail.com>
2023-06-30 11:17:15 -0300

    add CRUD request

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"gorm.io/gorm"
 )
 
-type album struct {
+type Album struct {
 	gorm.Model `json:"model"`
 	Title      string  `json:"title"`
 	Artist     string  `json:"artist"`
@@ -20,24 +20,37 @@ type apiContext struct {
 	db *gorm.DB
 }
 
-func (self *apiContext) getAlbums(c *gin.Context) {
-	var albums []album
-	result := self.db.Find(&albums)
+func (ctx *apiContext) getAlbuns(c *gin.Context) {
+	var albums []Album
+	result := ctx.db.Find(&albums)
 	if result.Error != nil {
 		c.AbortWithError(http.StatusInternalServerError, result.Error)
+		return
 	}
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
-func (self *apiContext) postAlbums(c *gin.Context) {
-	var newAlbum album
+func (ctx *apiContext) getAlbumById(c *gin.Context) {
+	var album Album
+	id := c.Params.ByName("id")
+	result := ctx.db.First(&album, id)
+
+	if result.Error != nil {
+		c.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+	c.IndentedJSON(http.StatusOK, album)
+}
+
+func (ctx *apiContext) postAlbum(c *gin.Context) {
+	var newAlbum Album
 
 	if err := c.BindJSON(&newAlbum); err != nil {
 		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
 
-	result := self.db.Create(&newAlbum)
+	result := ctx.db.Create(&newAlbum)
 
 	if result.Error != nil {
 		c.AbortWithError(http.StatusInternalServerError, result.Error)
@@ -46,29 +59,59 @@ func (self *apiContext) postAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, newAlbum)
 }
 
-// func Find(id string) *album {
-// 	for _, n := range albums {
-// 		if id == n.ID {
-// 			return &n
-// 		}
-// 	}
-// 	return nil
-// }
+func (ctx *apiContext) deleteAlbum(c *gin.Context) {
+	id := c.Params.ByName("id")
+	result := ctx.db.Delete(&Album{}, id)
+
+	if result.Error != nil {
+		c.AbortWithError(http.StatusNotFound, result.Error)
+		return
+	}
+	c.Status(http.StatusOK)
+}
+
+func (ctx *apiContext) updateAlbum(c *gin.Context) {
+	var album Album
+	var newAlbum Album
+
+	id := c.Params.ByName("id")
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	ctx.db.First(&album, id)
+
+	album.Title = newAlbum.Title
+	album.Artist = newAlbum.Artist
+	album.Price = newAlbum.Price
+
+	result := ctx.db.Save(&album)
+
+	if result.Error != nil {
+		c.AbortWithError(http.StatusInternalServerError, result.Error)
+	}
+
+	c.IndentedJSON(http.StatusOK, album)
+}
 
 func handleRequest(db *gorm.DB) {
 	ctx := apiContext{
 		db,
 	}
 	router := gin.Default()
-	router.GET("/albuns", ctx.getAlbums)
-	// router.GET("/albums/:id", getAlbumsById)
-	router.POST("/albuns", ctx.postAlbums)
+	router.GET("/albuns", ctx.getAlbuns)
+	router.GET("/albuns/:id", ctx.getAlbumById)
+	router.POST("/albuns", ctx.postAlbum)
+	router.DELETE("/albuns/:id", ctx.deleteAlbum)
+	router.PUT("/albuns/:id", ctx.updateAlbum)
 
-	router.Run("localhost:8080")
+	router.Run("localhost:9000")
 }
 
 func initialMigration(db *gorm.DB) {
-	db.AutoMigrate(&album{})
+	db.AutoMigrate(&Album{})
 }
 
 func main() {
